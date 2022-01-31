b5ea6697831558e5b795308766a660e17d1c0e63
Talha Dar <talhadar13579@gmail.com>
2022-01-31 17:46:13 +0500

    Adding React Hello world Files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1>
+      <span class='text_1'>Welcome, My name is Talha Dar.</span>
+      <span class='text_2'>Hello World!!! Using React.</span>
+    </h1>
+  );
+}
+
+export default App;
